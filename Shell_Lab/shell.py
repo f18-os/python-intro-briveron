f3ece835aa87478bcaef1f1407d5eb16519e6aae
briveron@miners.utep.edu <briveron@miners.utep.edu>
2018-09-17 20:24:29 -0600

    i keep working but its all a blur

--- Shell_Lab/shell.py
+++ Shell_Lab/shell.py
@@ -4,45 +4,52 @@ import os, sys, time, re
 
 pid = os.getpid()               # get and remember pid
 
-os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
+#os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
 
+args = ["wc", "cat"]
 
 leave=""
 leave=input("[Brian_Shell]$ ")
 
 while leave!="exit":
-    leave=input("[Brian_Shell]$ ")
+    leave=input("[Brian_Shell]$ ").split()
     print(leave)
     rc = os.fork()
 
+
+
+
+
+
+    args = ["wc", "p3-exec.py"]
+
+    os.close(1)                 # redirect child's stdout
+    sys.stdout = open("p4-output.txt", "w")
+    fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
+    os.set_inheritable(fd, True)
+
     if rc < 0:
-        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        #os.write(2, ("fork failed, returning %d\n" % rc).encode())
         sys.exit(1)
 
     elif rc == 0:                   # child
-        os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %
-                     (os.getpid(), pid)).encode())
-        args = ["wc", "p3-exec.py"]
-
-        os.close(1)                 # redirect child's stdout
-        sys.stdout = open("p4-output.txt", "w")
-        fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
-        os.set_inheritable(fd, True)
-        os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+        #os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %
+        #             (os.getpid(), pid)).encode())
+        #os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
 
         for dir in re.split(":", os.environ['PATH']): # try each directory in path
-            program = "%s/%s" % (dir, args[0])
+            program = "%s/%s" % (dir, leave[0])
             try:
-                os.execve(program, args, os.environ) # try to exec program
+                os.execve(program, leave[0:], os.environ) # try to exec program
             except FileNotFoundError:             # ...expected
                 pass                              # ...fail quietly
 
-        os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+        #os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
         sys.exit(1)                 # terminate with error
 
-    else:                           # parent (forked ok)
-        os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %
-                     (pid, rc)).encode())
-        childPidCode = os.wait()
-        os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
-                     childPidCode).encode())
+    #else:                           # parent (forked ok)
+        #os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %
+        #             (pid, rc)).encode())
+        #childPidCode = os.wait()
+        #os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
+        #             childPidCode).encode())
