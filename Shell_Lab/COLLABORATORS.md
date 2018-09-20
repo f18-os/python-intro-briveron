2b5faa589927a45265f36d71f6f77c0325bf14cd
briveron@miners.utep.edu <briveron@miners.utep.edu>
2018-09-19 22:11:53 -0600

    Coming close to 10:30 so turning in

--- Shell_Lab/COLLABORATORS.md
+++ Shell_Lab/COLLABORATORS.md
@@ -1,4 +1,8 @@
-Francisco Antunez and Miguel Nunez have helped me with this lab due to
+Francisco Antunez, Alexis Reyes, Adrian, Sebastian Nunez, and Miguel Nunez
+have helped me with this lab due to
 my inexperience with python and the confusion that came with the assignment.
 I was lost with what was really being asked for the lab and was looking for
-someone to shed some light on this lab.
+someone to shed some light on this lab. Francisco helped me with the statements
+that would let me do my individual task such as when to pipe and when to Redirect
+The others helped me error check simple mistakes and errors i didn't noticed conflicted 
+with my code.
--- Shell_Lab/shell.py
+++ Shell_Lab/shell.py
@@ -4,26 +4,86 @@ import os, sys, time, re
 
 pid = os.getpid()               # get and remember pid
 
-#os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
 
-
+import fileinput
 
 leave=""
-leave=input("[Brian_Shell]$ ")
 
 
 def isPipe(userIn):
     for i, Eric in enumerate(userIn):
-        if Eric== '|':
+        if Eric == '|':
             #print("I see the |")
             left,right = userIn.split('|')
 
+            pr,pw = os.pipe()
+            for f in (pr, pw):
+                os.set_inheritable(f, True)
+            print("pipe fds: pr=%d, pw=%d" % (pr, pw))
+
+            rc = os.fork()
 
 
+            if rc == 0:                   #  child - will write to pipe
+                print("Child: My pid==%d.  Parent's pid=%d" % (os.getpid(), pid), file=sys.stderr)
+                args = ["wc", "p3-exec.py"]
 
+                os.close(1)                 # redirect child's stdout
+                os.dup(pw)
+                for fd in (pr, pw):
+                    os.close(fd)
+                print("hello from child")
 
+            else:                           # parent (forked ok)
+                print("Parent: My pid==%d.  Child's pid=%d" % (os.getpid(), rc), file=sys.stderr)
+                os.close(0)
+                os.dup(pr)
+                for fd in (pw, pr):
+                    os.close(fd)
+                for line in fileinput.input():
+                    print("From child: <%s>" % line)
 
+def isRedirect(userIn):
+    for i, Eric in enumerate(userIn):
+        if Eric == '<':
+            print("Do this <")
 
+        elif Eric == '>':
+            print("Do this >")
+
+        os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
+
+        rc = os.fork()
+
+        if rc < 0:
+            os.write(2, ("fork failed, returning %d\n" % rc).encode())
+            sys.exit(1)
+
+        elif rc == 0:                   # child
+            os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %
+                         (os.getpid(), pid)).encode())
+            args = ["wc", "p3-exec.py"]
+
+            os.close(1)                 # redirect child's stdout
+            sys.stdout = open("p4-output.txt", "w")
+            os.set_inheritable(1, True)
+
+            for dir in re.split(":", os.environ['PATH']): # try each directory in path
+                program = "%s/%s" % (dir, args[0])
+                try:
+                    os.execve(program, args, os.environ) # try to exec program
+                except FileNotFoundError:             # ...expected
+                    pass                              # ...fail quietly
+
+            os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+            sys.exit(1)                 # terminate with error
+
+        else:                           # parent (forked ok)
+            os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %
+                         (pid, rc)).encode())
+            childPidCode = os.wait()
+            os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
+                         childPidCode).encode())
 
 
 while leave!="exit":
@@ -31,55 +91,15 @@ while leave!="exit":
     print(leave)
     rc = os.fork()
 
-    args = leave
+    copylist = leave
 
     if rc < 0:
         sys.exit(1)
 
-
-    for i, args in enumerate(args):
-        if args[i]== '|':
-            #print("I see the |")
-            left,right = args.split('|')
-            print(left, "|" ,right)
+    os.close(1)
 
 
+    isPipe(leave)
 
-
-
-
-
-
-
-
-
-
-
-    os.close(1)                 # redirect child's stdout
-    sys.stdout = open("p4-output.txt", "w")
-    fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
-    os.set_inheritable(fd, True)
-
-
-
-    elif rc == 0:                   # child
-        #os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %
-        #             (os.getpid(), pid)).encode())
-        #os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
-
-        for dir in re.split(":", os.environ['PATH']): # try each directory in path
-            program = "%s/%s" % (dir, leave[0])
-            try:
-                os.execve(program, args, os.environ) # try to exec program
-            except FileNotFoundError:             # ...expected
-                pass                              # ...fail quietly
-
-        #os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
-        sys.exit(1)                 # terminate with error
-
-    #else:                           # parent (forked ok)
-        #os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %
-        #             (pid, rc)).encode())
-        #childPidCode = os.wait()
-        #os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
-        #             childPidCode).encode())
+    copylist.split(" ")
+    isRedirect(leave)
